bc6e584dff213b85e1bec2025427442436204f5d
lillywolf <lillywolf@gmail.com>
2011-11-08 18:52:17 -0800

    moved files

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source :gemcutter
+gem 'rails', '2.3.11'
+gem 'rack', '1.1.0'
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '3d0692d7863459d39e01443b26844fd386a56b5c6e5df4edf13dbdf1cd9bb07d9b26de7328719001aa1e44abad0a13ce5823747022e9703f0562f1f7a7f33c9b';
